f27d830385827ed02717f7e85e36d90f5be8d0bf
Erica Sadun <erica@ericasadun.com>
2018-08-02 08:00:43 -0600

    Added usage message

--- hello.c
+++ hello.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char **argv) {
+
+  if (argc > 1) {
+    printf("Usage: %s\n", argv[0]);
+    exit(-1);
+  }
+
   printf("Hello world!\n");
+
 }
